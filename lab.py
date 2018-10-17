59cb982a5d1f3e274a52cdb87c5b2e3534a08d59
itay20-meet <itay20@meet.mit.edu>
2018-10-17 12:25:37 -0400

    y first commit

---
+++ lab.py
@@ -0,0 +1,42 @@
+#print(100*"itay")
+#number1=19.0
+#print(number1)
+#number2=number1/2
+#number3=1
+#print(number2)
+#list1=[1,2,3]
+#total=0
+#print (list1)
+#for i in range(3):
+	#print(list1[i],list1[i]*2)
+	#total=total+list1[i]
+#print(total)
+import turtle
+#turtle.goto(100,100)
+#turtle.clear()
+#turtle.begin_fill()
+#turtle.goto(100,200)
+#turtle.goto(200,200)
+#turtle.goto(200,100)
+#turtle.goto(100,100)
+#turtle.end_fill()
+turtle.pensize(15)
+turtle.circle(90)
+turtle.penup()
+turtle.goto(-220,0)
+turtle.pendown()
+turtle.color("blue")
+turtle.circle(90)
+turtle.penup()
+turtle.goto(220,0)
+turtle.pendown()
+turtle.color("red")
+turtle.circle(90)
+turtle.penup()
+turtle.goto(175,-45)
+
+	
+
+
+turtle.mainloop()
+
---
+++ lab2.py
@@ -0,0 +1,29 @@
+def listpart2():
+	y=[x[0],x[-1]]
+	print (y)
+list1=[1,2,3]
+#listpart2()
+
+list2=[1,2,3,4,5,6,55]
+def numbersless5():
+	y=0
+	while x[y]<5:
+		print x[y]
+		y=y+1
+#numbersless5()
+list3 = []
+def commonum():
+	for x in list1:
+		for y in list2:
+			if y==x:
+				list3.append(x)
+	print(list3)					
+commonum()	
+	
+	
+
+
+
+
+
+		
