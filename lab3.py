5472525869c34f183ca7c69a59004e65f2cd8245
itay20-meet <itay20@meet.mit.edu>
2018-10-31 13:18:20 -0400

    lab 4

---
+++ lab3.py
@@ -0,0 +1,14 @@
+import turtle
+#turtle.addshape("cat.gif")
+#turtle.shape("cat.gif")
+#turtle.getshapes()
+turtle.speed(0)
+for i in range(1000):
+	 
+	turtle.forward(200)
+	turtle.right(45)
+	turtle.forward(100)
+	turtle.right(90)
+	turtle.forward(50)
+	turtle.home()
+	turtle.right(i)
---
+++ lab4.py
@@ -0,0 +1,30 @@
+x=5
+class animal():
+	def __init__(self,sound,name,age,favorite_color):
+		self.sound = sound
+		self.name = name
+		self.age = age
+		self.favorite_color = favorite_color
+	def eat(self,food):
+		print("Yummy " +self.name +"is eating " + food)
+	def description(self):
+		print(self.name + "is" + self.age +"years old and loves the color" + self.favorite_color)
+	def make_sound(self, sound):
+		print(sound*x)
+animal = animal("moo", "otis", "11", "white")
+#animal.eat("burger")
+#animal.description()
+animal.make_sound("moo")
+class person():
+	def __init__(self,name,age,hight,city,gender, favorite_breakfast):
+		self.name = name
+		self.age = age
+		self.hight = hight
+		self.city = city
+		self.gender = gender
+		self.favorite_breakfast = favorite_breakfast
+	def eat_breakfast(self, favorite_breakfast):
+		print(self.name + "is eating" + self.favorite_breakfast )	
+itay = person("Itay","15", "171", "Jerusalem", "male")
+itay.eat_breakfast("eggs")
+
